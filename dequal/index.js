26b3b256a2a160045782285fd3f7eab8cb5aa80c
wenhu <wenhu@myweimai.com>
2021-12-27 17:57:52 +0800

    feat: update

--- dequal/index.js
+++ dequal/index.js
@@ -5,13 +5,17 @@
  */
 
 const deepEqual = (src, src1) => {
+  // 粗类型判断，不一致直接 false
   if (typeof src !== typeof src1) {
     return false;
   }
   if (typeof src === "object" && typeof src1 === "object") {
     if (Array.isArray(src) && Array.isArray(src1)) {
-      for (let key in src) {
-        if (!deepEqual(src[key], src1[key])) {
+      if (src.length !== src1.length) {
+        return false;
+      }
+      for (let i = 0, len = src.length; i < len; i++) {
+        if (!deepEqual(src[i], src1[i])) {
           return false;
         }
         return true;
